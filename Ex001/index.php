257bbf78a454623980884b6a9d51fc1dac0602d2
ManoUpdev <emanuelcgalindo@hotmail.com>
2025-05-20 19:36:30 -0300

    first php files

---
+++ Ex001/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible">
+    <meta name = "viewpoint">
+    <title>xablau</title>      
+    <body>
+        <p>
+
+        </p>
+    </body>
+</head>
+</html>
