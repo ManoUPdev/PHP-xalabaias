d67c768d6189ce6c98fca7df3a54665b3b845b51
ManoUpdev <emanuelcgalindo@hotmail.com>
2025-05-20 20:41:51 -0300

    ex003 initial

---
+++ Ex003/index.php
@@ -0,0 +1,16 @@
+<?php 
+   $nome = "Rafael";
+   $sobrenome = "Pires de Mello";
+   const PAIS = "Brasil";
+   //
+   //
+   //
+   //
+
+   //
+   //
+   //
+   //
+
+   echo " Muito prazer, $nome $sobrenome! Voce mora no " .PAIS; 
+?>
